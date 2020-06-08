b52e01e818165bc4ad00c4b55aed71a36011563d
Tejas Cousik <tcousik0@gmail.com>
2020-06-08 16:50:28 -0600

    auto scroll to bottom

--- src/chatview/chatView.js
+++ src/chatview/chatView.js
@@ -3,15 +3,24 @@ import styles from "./styles";
 import { withStyles } from "@material-ui/core/styles";
 
 class ChatView extends React.Component {
+  componentDidUpdate = () => {
+    const container = document.getElementById("chatview-container");
+    if (container) {
+      container.scrollTo(0, container.scrollHeight);
+    }
+  };
+
   render() {
     const { classes, chat, user } = this.props;
     if (chat === undefined) {
-      return <main className={classes.content}></main>;
+      return <main id="chatview-container" className={classes.content}></main>;
     } else {
       return (
         <div>
-          <div></div>
-          <main className={classes.content}>
+          <div className={classes.chatHeader}>
+            Your conversation with {chat.users.filter((_usr) => _usr !== user)}
+          </div>
+          <main id="chatview-container" className={classes.content}>
             {chat.messages.map((_msg, _index) => {
               return (
                 <div
